fa51bacc30133dcb686dbddee0cfc9585ddfb061
tiger <nazrulislamrakib1813030@gmail.com>
2025-05-06 16:43:17 +0600

    Initial commit

---
+++ product/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import Product
+from rest_framework import response
+# Create your views here.
+@api_view(['GET'])
+def get_products(request):
+    all_products=Product.objects.all()
+    print(all_products)
+    return response("Hello Rakib")
