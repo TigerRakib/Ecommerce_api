87baf4cfed5714f9ac1a5d6089ee597f4bfd3313
tiger <nazrulislamrakib1813030@gmail.com>
2025-05-12 00:36:31 +0600

    "Add Pagination to the product api"

--- product/views.py
+++ product/views.py
@@ -4,13 +4,23 @@ from .models import Product
 from .serializers import Productserializer
 from rest_framework.response import Response
 from .filters import ProductsFilter
+from rest_framework.pagination import PageNumberPagination
 # Create your views here.
 @api_view(['GET'])
 def get_products(request):
     all_products=Product.objects.all()
     filterset= ProductsFilter(request.GET, queryset=all_products.order_by('id'))
-    serializer=Productserializer(filterset.qs, many=True)
-    return Response(serializer.data)
+    count=filterset.qs.count()
+    #pagination
+    resPerPage=1
+    paginator=PageNumberPagination()
+    paginator.page_size=resPerPage
+    query_set= paginator.paginate_queryset(filterset.qs, request)
+    serializer=Productserializer(query_set, many=True)
+    return Response({
+        "count":count,
+        "resperpage":resPerPage,
+        "products":serializer.data})
 
 @api_view(["GET"])
 def get_product(request,pk):
